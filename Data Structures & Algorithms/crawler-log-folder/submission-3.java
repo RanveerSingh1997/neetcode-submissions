15faa6c3776a6686813eb1917619f74e27d12ed0
Ranveer Singh Gour <31967062+RanveerSingh1997@users.noreply.github.com>
2026-09-04 18:40:44 +0530

    Add: crawler-log-folder - submission-3

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        Stack<String> stack=new Stack();
+        String root=logs[0];
+        for(String str:logs){
+            if(str.equals("../")){
+                 if(!stack.isEmpty()){
+                     stack.pop();
+                 }   
+            }else if(!str.equals("./")){
+                stack.push(str);
+            }
+        }
+        return stack.size();
+    }
+}
